ed6dd794bf2509fc5ce8a8e4ebfb6e2ae3cdb29e
Ezequiel Deniz <ezedeniz10@hotmail.com>
2023-07-08 11:26:56 -0300

    Creada clase Persona

---
+++ Prueba Git/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_Git
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Saludar()
+        {
+            return $"hola mi nombre es {Nombre} y mi apellido es {Apellido}";
+        }
+    }
+}
